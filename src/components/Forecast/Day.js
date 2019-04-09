1d4a0ce4cb236683a4e92d3d8d9e535f39820cdc
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2019-04-09 01:23:32 -0400

    Create Day component

---
+++ src/components/Forecast/Day.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Day = props => {
+	return (
+		<div className="card">
+			<div className="item1">
+				{props.data.day}
+			</div>
+
+			<div className="item2">
+				{props.data.condition}
+			</div>
+
+			<div className="item3">
+				{props.data.high}{' '}{props.data.low}
+			</div>
+		</div>
+	)
+};
